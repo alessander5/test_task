c5e2a1fd81805da28177417483d954360e5c3f14
alessander5 <alessander4@gmail.com>
2017-09-19 15:46:35 +0200

    4.1 :: Angular training

---
+++ view/task1/hello.js
@@ -0,0 +1,9 @@
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope) {
+    $scope.greeting = "";
+    $scope.update = function(){
+        if($scope.name){
+            $scope.greeting = "Hello, " + $scope.name+"!";
+        }
+    }
+});
---
+++ view/task2_2/public/hello.js
@@ -0,0 +1,22 @@
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope, $http) {
+    $scope.greeting = "";
+    $scope.update = function(){
+        if($scope.name){
+            $http.get("/greeting",
+                {params:
+                    {name : $scope.name}
+                })
+                .then(function(res){
+                    $scope.greeting = res.data;
+                });
+        }
+    }
+
+    $scope.$watch("name", function(){
+        $scope.update();
+    });
+
+    // $interval($scope.update, 200);
+
+});
